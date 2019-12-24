4067a6bf9e70f3227bf65b055413e9ca22201429
liulei3 <liulei3@guahao.com>
2019-12-24 17:01:42 +0800

    add static

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
+const port = 3001;
+
+const static = express.static('static');
+app.use(static);
+
 app.get('/', (req, res) => {
     res.send('Hello Express!');
 });
 
-app.listen(3000, () => {
-    console.log('示例应用正在监听 3000 端口！')
+app.listen(port, () => {
+    console.log('示例应用正在监听 ${port} 端口！')
 });
