d1fa77c60273a075e272a09fe9d7cd812f475289
liulei3 <liulei3@guahao.com>
2019-12-26 10:38:27 +0800

    port -> 3007

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 3006;
+const port = 3007;
 
 const static = express.static('static');
 app.use(static);
--- static/lets/index.js
+++ static/lets/index.js
@@ -8,28 +8,29 @@ var chart = d3.select(".chart")
     .attr("width", width);
 
 d3.tsv("data.tsv", type, function(error, data) {
+    console.log(error);
     console.log(data);
 
-    x.domain([0, d3.max(data, function(d) { return d.value; })]);
+    // x.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-    chart.attr("height", barHeight * data.length);
+    // chart.attr("height", barHeight * data.length);
 
-    var bar = chart.selectAll("g")
-        .data(data)
-        .enter().append("g")
-        .attr("transform", function(d, i) {
-            return "translate(0," + i * barHeight + ")";
-        });
+    // var bar = chart.selectAll("g")
+    //     .data(data)
+    //     .enter().append("g")
+    //     .attr("transform", function(d, i) {
+    //         return "translate(0," + i * barHeight + ")";
+    //     });
 
-    bar.append("rect")
-        .attr("width", function(d) { return x(d.value); })
-        .attr("height", barHeight - 1);
+    // bar.append("rect")
+    //     .attr("width", function(d) { return x(d.value); })
+    //     .attr("height", barHeight - 1);
 
-    bar.append("text")
-        .attr("x", function(d) { return x(d.value) - 15; })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")  // dy ?
-        .text(function(d) { return d.value; });   
+    // bar.append("text")
+    //     .attr("x", function(d) { return x(d.value) - 15; })
+    //     .attr("y", barHeight / 2)
+    //     .attr("dy", ".35em")  // dy ?
+    //     .text(function(d) { return d.value; });   
 });
 
 function type(d) {
