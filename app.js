b3b367ccf2771d15cadac725d5070b084ca0f290
liulei3 <liulei3@guahao.com>
2020-01-01 18:26:10 +0800

    modify

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 3016;
+const port = 3017;
 
 const static = express.static('static');
 app.use(static);
--- static/mine/index.js
+++ static/mine/index.js
@@ -102,9 +102,12 @@ export default function define(runtime, observer) {
 
                 let bars = svg.selectAll('.bar').data(yearSlice, d => d.name);
 
+                // bar 从表格外加入表格，初始显示在表格外，使用动画显示到它应显示的位置
+                // ease 后的attr 表格希望动画后rect的属性
                 bars.enter()
                     .append('rect')
                     .attrs({
+                        // 空格换为下划线
                         class: d => `bar ${d.name.replace(/\s/g,'_')}`,
                         x: x(0)+1,
                         width: d => x(d.value)-x(0)-1,
@@ -121,6 +124,7 @@ export default function define(runtime, observer) {
                         y: d => y(d.rank) + 5
                     });
 
+                // 表格中已存在的bar，在数据变化后，变更它的width和y，使用动画移动它的位置
                 bars
                     .transition()
                     .duration(tickDuration)
@@ -130,6 +134,7 @@ export default function define(runtime, observer) {
                         y: d => y(d.rank)+5
                     });
 
+                // 因数据变化，需要移出表格的bar，指定它需要变化到的width和y，然后使用动画移动
                 bars
                     .exit()
                     .transition()
@@ -140,6 +145,123 @@ export default function define(runtime, observer) {
                         y: d => y(top_n+1)+5
                     })
                     .remove();
+
+
+                let labels = svg.selectAll('.label').data(yearSlice, d => d.name);
+
+                labels
+                    .enter()
+                    .append('text')
+                    .attrs({
+                        class: 'label',
+                        transform: d => `translate(${x(d.value)-5}, ${y(top_n+1)+5+((y(1)-y(0))/2)-8})`,
+                        'text-anchor': 'end'
+                    })
+                    .html('')
+                    .transition()
+                    .duration(tickDuration)
+                    .ease(d3.easeLinear)
+                    .attrs({
+                        transform: d => `translate(${x(d.value)-5}, ${y(d.rank)+5+((y(1)-y(0))/2)-8})`
+                    });
+
+                let tspans = labels.selectAll('tspan')
+                    .data(d => [{text: d.name, opacity: 1, weight:600}, {text: d.subGroup, opacity: 1, weight:400}]);
+
+                tspans.enter()
+                    .append('tspan')
+                    .html(d => d.text)
+                    .attrs({
+                        x: 0,
+                        dy: (d,i) => i*16
+                    })
+                    .styles({
+                        // opacity: d => d.opacity,
+                        fill: d => d.weight == 400 ? '#444444':'',
+                        'font-weight': d => d.weight,
+                        'font-size': d => d.weight == 400 ? '12px':''
+                    });
+
+                tspans
+                    .html(d => d.text)
+                    .attrs({
+                        x: 0,
+                        dy: (d,i) => i*16
+                    })
+                    .styles({
+                        // opacity: d => d.opacity,
+                        fill: d => d.weight == 400 ? '#444444':'',
+                        'font-weight': d => d.weight,
+                        'font-size': d => d.weight == 400 ? '12px':''
+                    });
+
+                tspans.exit().remove();
+
+                labels
+                    .transition()
+                    .duration(tickDuration)
+                    .ease(d3.easeLinear)
+                    .attrs({
+                        transform: d => `translate(${x(d.value)-5}, ${y(d.rank)+5+((y(1)-y(0))/2)-8})`
+                    });
+
+                labels
+                    .exit()
+                    .transition()
+                    .duration(tickDuration)
+                    .ease(d3.easeLinear)
+                    .attrs({
+                        transform: d => `translate(${x(d.value)-8}, ${y(top_n+1)+5})`
+                    })
+                    .remove();
+
+                let valueLabels = svg.selectAll('.valueLabel').data(yearSlice, d => d.name);
+
+                valueLabels
+                    .enter()
+                    .append('text')
+                    .attrs({
+                      class: 'valueLabel',
+                      x: d => x(d.value)+5,
+                      y: d => y(top_n+1)+5,
+                    })
+                    .text(d => d3.format(',.0f')(d.lastValue))
+                    .transition()
+                    .duration(tickDuration)
+                    .ease(d3.easeLinear)
+                    .attrs({
+                        y: d => y(d.rank)+5+((y(1)-y(0))/2)+1
+                    });
+
+                valueLabels
+                    .transition()
+                    .duration(tickDuration)
+                    .ease(d3.easeLinear)
+                    .attrs({
+                        x: d => x(d.value)+5,
+                        y: d => y(d.rank)+5+((y(1)-y(0))/2)+1
+                    })
+                    .tween("text", function(d) {
+                        let i = d3.interpolateRound(d.lastValue, d.value);
+                        return function(t) {
+                            this.textContent = d3.format(',')(i(t));
+                        };
+                    });
+
+                valueLabels
+                    .exit()
+                    .transition()
+                    .duration(tickDuration)
+                    .ease(d3.easeLinear)
+                    .attrs({
+                        x: d => x(d.value)+5,
+                        y: d => y(top_n+1)+5
+                    })
+                    .remove();
+
+
+
+
 
                 if(year == 2018) ticker.stop();    // 2018 == endYear
                 year = year + 1;
