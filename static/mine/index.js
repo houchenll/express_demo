766e326bb831e7b1d193a54bb5c27ec03dafe5b9
liulei3 <liulei3@guahao.com>
2020-01-01 16:13:46 +0800

    modify

--- static/mine/index.js
+++ static/mine/index.js
@@ -11,6 +11,11 @@ export default function define(runtime, observer) {
         return ('world');
     });
 
+    main.variable(observer("dataset")).define("dataset", ["d3"], function(d3) {
+        console.log("dataset observer");
+        return(d3.csv('https://gist.githubusercontent.com/johnburnmurdoch/4199dbe55095c3e13de8d5b2e5e5307a/raw/fa018b25c24b7b5f47fd0568937ff6c04e384786/city_populations'))
+    });
+
     console.log("return main");
     return main;
 }
